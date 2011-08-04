5995e66d650abed9d40757dc7b8c2511c52e1175
Rony Shapiro <ronys@acm.org>
2011-08-04 22:49:53 +0300

    Refactoring to common Money class, WIP

--- Franc.h
+++ Franc.h
@@ -1,9 +1,9 @@
-class Franc {
+#include "Money.h"
+
+class Franc : public Money {
  public:
- Franc(int amount) : amount(amount) {}
+ Franc(int amount) : Money(amount) {}
   Franc times(int multiplier) {return Franc(amount * multiplier);}
   bool operator==(const Franc &lhs) const {return lhs.amount == this->amount;}
   bool operator!=(const Franc &lhs) const {return !(lhs == *this);}
- private:
-  int amount;
 };
