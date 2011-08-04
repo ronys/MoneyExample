31ff5e6552386325713e3c57cfa43f9c96072fad
Rony Shapiro <ronys@acm.org>
2011-08-04 22:13:35 +0300

    Builds and fails.

--- Dollar.h
+++ Dollar.h
@@ -4,4 +4,5 @@ class Dollar {
   Dollar times(int multiplier) {return Dollar(amount * multiplier);}
   int amount;
   bool operator==(const Dollar &) const {return true;}
+  bool operator!=(const Dollar &lhs) const {return !(lhs == *this);}
 };
