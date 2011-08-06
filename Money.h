4de1cb1baf0cf067e6a9347c4b401c3b8082fe36
Rony Shapiro <ronys@acm.org>
2011-08-06 08:15:09 +0300

    Added print Money functionality to help uderstand failure

--- Money.h
+++ Money.h
@@ -1,6 +1,6 @@
 #ifndef __MONEY_H
 #define __MONEY_H
-
+#include <iostream>
 class Bank;
 
 class Money
@@ -16,11 +16,21 @@ class Money
   bool operator!=(const Money &rhs) const {return !(rhs == *this);}
 
   Money times(int multiplier) {return Money(m_amount * multiplier, m_currency);}
-
+  friend std::ostream &operator<<(std::ostream &os, const Money &m);
  private:
   int m_amount;
   CURRENCY m_currency;
   friend class Bank;
 };
+
+inline std::ostream &operator<<(std::ostream &os, const Money &m)
+{
+  switch (m.m_currency) {
+  case Money::USD: os << "$ "; break;
+  case Money::CHF: os << "CHF "; break;
+  }
+  os << m.m_amount;
+  return os;
+}
 #endif /* __MONEY_H */
 
