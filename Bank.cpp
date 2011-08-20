79f2a9d6b69970cc43db89cbe610e30c6cf11c33
ronys <ronys@users.sourceforge.net>
2011-08-20 21:27:22 +0300

    Added ConvertCurrency test, red

--- Bank.cpp
+++ Bank.cpp
@@ -4,3 +4,14 @@ Money Bank::convert(const Expr &value, Money::CURRENCY currency)
 {
   return Money(value.m_m1.m_amount + value.m_m2.m_amount, Money::USD);
 }
+
+Money Bank::convert(const Money &value, Money::CURRENCY currency)
+{
+  return value;
+}
+
+void Bank::addRate(Money::CURRENCY from, Money::CURRENCY to, int rate)
+{
+}
+
+
