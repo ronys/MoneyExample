f24561ac4441f77d5cdcb9ee93b1170500aead22
ronys <ronys@users.sourceforge.net>
2011-08-20 13:21:55 +0300

    Added test for new currency

--- MEtest.cpp
+++ MEtest.cpp
@@ -6,12 +6,20 @@
 TEST(MoneyTest, Multiplication) {
   Dollar five(5);
   Dollar product = five.times(2);
-  EXPECT_EQ(10, product.amount);
+  EXPECT_EQ(Dollar(10), product);
   product = five.times(3);
-  EXPECT_EQ(15, product.amount);
+  EXPECT_EQ(Dollar(15), product);
 }
 
 TEST(MoneyTest, Equality) {
   EXPECT_EQ(Dollar(5), Dollar(5));
   EXPECT_NE(Dollar(5), Dollar(6));
+}
+
+TEST(MoneyTest, FrancMultiplication) {
+  Franc five(5);
+  Franc product = five.times(2);
+  EXPECT_EQ(Franc(10), product);
+  product = five.times(3);
+  EXPECT_EQ(Franc(15), product);
 }
